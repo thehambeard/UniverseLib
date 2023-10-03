16b9c56456e4c2b24317ed93baf3277954ece36e
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-10-03 10:56:49 +0900

    backport:bydock melon changes

--- src/Reflection/Il2CppReflection.cs
+++ src/Reflection/Il2CppReflection.cs
@@ -140,11 +140,24 @@ namespace UniverseLib
             if (obfuscatedToDeobfuscatedTypes.TryGetValue(fullname, out Type deob))
                 return deob;
 
+#if ML
+            // The new melonloader prepends Il2Cpp in front of most namespaces
+            bool isUnityType = fullname.StartsWith("Unity") && fullname.Contains('.');
+            bool isInjected = "InjectedMonoTypes".Equals(cppType.Assembly.GetName().Name);
+            if (!isUnityType && !isInjected)
+            {
+                if (fullname.Contains('.'))
+                    fullname = $"Il2Cpp{fullname}";
+                else
+                    fullname = $"Il2Cpp.{fullname}";
+            }
+#else
             // An Il2CppType cannot ever be a System type.
             // Unhollower returns Il2CppSystem types and System for some reason.
             // Let's just manually fix that.
             if (fullname.StartsWith("System."))
                 fullname = $"Il2Cpp{fullname}";
+#endif
 
             if (!AllTypes.TryGetValue(fullname, out Type monoType))
             {
