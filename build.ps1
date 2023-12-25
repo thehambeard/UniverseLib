e6e3ed390127c8479a1f67c510b51fbb476cacff
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-12-25 13:55:43 +0900

    fix:build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
-dotnet build .\src\UniverseLib.sln -c Release_IL2CPP_Unhollower
+dotnet build .\src\UniverseLib.sln -c Release_IL2CPP_Interop_BIE
 
-dotnet build .\src\UniverseLib.sln -c Release_IL2CPP_Interop
+dotnet build .\src\UniverseLib.sln -c Release_IL2CPP_Interop_ML
+
+dotnet build .\src\UniverseLib.sln -c Release_IL2CPP_Unhollower
 
 dotnet build .\src\UniverseLib.sln -c Release_Mono
